5d104fb595c808653c78356442f26fdc422f42d3
ekkusa <dayesofficial@gmail.com>
2024-01-17 17:32:50 +0000

    apollo: xz2c -> apollo
    Might aswell revert back the name

--- lineage_apollo.mk
+++ lineage_apollo.mk
@@ -4,19 +4,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from xz2c device
-$(call inherit-product, device/sony/xz2c/device.mk)
+# Inherit from apollo (sony) device
+$(call inherit-product, device/sony/apollo/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_xz2c
-PRODUCT_DEVICE := xz2c
+PRODUCT_NAME := lineage_apollo
+PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := Xperia XZ2 Compact
 
-PRODUCT_GMS_CLIENTID_BASE := android-sony
+PRODUCT_GMS_CLIENTID_BASE := android-sony-mobile
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="H8324" \
